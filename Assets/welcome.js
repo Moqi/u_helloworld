dc6fd5b0a49c5586ec7671523617aa9c368b8576
jeiry <v_f@21cn.com>
2013-11-26 17:44:53 +0800

    sdfg

---
+++ Assets/welcome.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var str : String;
+var imageTexture : Texture;
+private var imageWidth : int;
+private var imageHeight : int;
+private var screenWidth : int;
+private var screenHeight : int;
+function Start () {
+	imageWidth = Screen.width;
+	screenHeight = Screen.height;
+	
+	imageWidth = imageTexture.width;
+	imageHeight = imageTexture.height;
+}
+
+function OnGUI(){
+	GUI.Label(Rect(100,10,100,30),str);
+	GUI.Label(Rect(100,120,imageWidth,imageHeight),imageTexture);
+}
+
+function Update () {
+
+}
